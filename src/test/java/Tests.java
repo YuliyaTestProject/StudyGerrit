e5495de9e1cf08b1f04467668bb473e1d375ff91
Julia <julia.jgoo@gmail.com>
2018-05-20 15:43:27 +0300

    Test2

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,6 +1,11 @@
 import Portal.EventsPage;
+import Portal.TopMenuPage;
+import Portal.WidgetsPage;
+import org.openqa.selenium.WebDriver;
 
 public class Tests {
 
+
     EventsPage event = new EventsPage();
+    WidgetsPage widget = new WidgetsPage();
 }
